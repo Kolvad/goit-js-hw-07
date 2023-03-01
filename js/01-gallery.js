2409f34aece939339b88b70f860a2c8714230b69
Kolvad <vskolesnik@gmail.com>
2023-03-01 16:24:02 +0200

    fixed hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${element.original}">
+                <img
+                    class="gallery__image"
+                    src="${element.preview}"
+                    data-source="${element.original}"
+                    alt="${element.description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join("");
+}
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+`);
+
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `
+        <li>
+          <a class="gallery__item" href="${element.original}">
+            <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+          </a>
+        </li>   
+      `;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
